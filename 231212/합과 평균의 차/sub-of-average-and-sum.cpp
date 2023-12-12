826053a1a244cb047545c80359a43521df28cf45
BROWNPU9 <137032025+BrownLEE99@users.noreply.github.com>
2023-12-12 16:39:47 +0900

    Title: 합과 평균의 차, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 231212/합과 평균의 차/sub-of-average-and-sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cin >> a >> b >> c;
+    int sum = a+b+c;
+    int mid = sum/3;
+    int last = sum - mid;
+    cout << sun << "\n" << mid << "\n" << last;
+    return 0;
+}
