09b49dac3df3d673ee80ab10720ecb8c9c6b008b
BROWNPU9 <137032025+BrownLEE99@users.noreply.github.com>
2023-12-31 13:00:59 +0900

    Title: 용량이 다른 3개의 물통, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231231/용량이 다른 3개의 물통/three-water-bottles-with-different-capacities.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<pair<int,int>> bucket;
+int main() {
+
+    for(int i = 0; i < 3; i ++){
+        int a,b;
+        cin >> a >> b;
+        bucket.push_back({a,b});
+    }
+    int cnt = 0;
+    int totalCnt = 0;
+    while(true) {
+        totalCnt++;
+        int next = (cnt+1)%3;
+        int nextLimit = bucket[next].first;
+        // 넣을 수 있는 양
+        int nextLast = bucket[next].first - bucket[next].second;
+        if(nextLast > 0){
+            if(nextLast >= bucket[cnt].second) {
+                bucket[next].second += bucket[cnt].second;
+                bucket[cnt].second = 0;
+            } else {
+                int gap = bucket[cnt].second - nextLast;
+                bucket[next].second = bucket[next].first;
+                bucket[cnt].second = gap;
+            }
+        }
+        cnt = next;
+        if(totalCnt ==  301) break;
+    }
+    for(int i = 0; i < 3; i++){
+        cout << bucket[i].second << "\n";
+    }
+    return 0;
+}
